a5bef781a0d09b4f8bd87ee8c1a1050727e0f5ff
윤예리 <yoonr721@gmail.com>
2024-01-31 22:07:33 +0900

    0131_home

--- 02_algorithm/99_Extra/1w_Extra/1979_어디에_단어가_들어갈수_있을까/sol.py
+++ 02_algorithm/99_Extra/1w_Extra/1979_어디에_단어가_들어갈수_있을까/sol.py
@@ -1,3 +1,15 @@
 import sys
 sys.stdin = open('input.txt')
 
+T = int(input())
+for test_case in range(1, T+1):
+    N, K = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    cnt = 0
+    for i in range(N):
+        for j in range(N):
+            # 행 탐색
+
+
+
