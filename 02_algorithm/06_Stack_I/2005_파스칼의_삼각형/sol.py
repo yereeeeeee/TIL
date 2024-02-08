f4c4e40d815eb4e06fdfea34ce587885b163f9f6
윤예리 <yoonr721@gmail.com>
2024-02-08 14:45:41 +0900

    0208

--- 02_algorithm/06_Stack_I/2005_파스칼의_삼각형/sol.py
+++ 02_algorithm/06_Stack_I/2005_파스칼의_삼각형/sol.py
@@ -1,8 +1,29 @@
 import sys
 sys.stdin = open('input.txt')
 
+# 메모리 에러
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
 
-    def
+    def p_tri(n):
+        global memo
+        if n >= 2:
+            result = []
+            a = []
+            for i in range(n-1):
+                a.append(memo[n-1][i]+memo[n-1][i+1])
+            result.append(1)
+            result.extend(a)
+            result.append(1)
+            memo[n] = result
+            return memo[n]
+
+    memo = [0] * (N)
+    memo[0] = [1]
+    memo[1] = [1, 1]
+
+    print(f'#{tc}')
+    for i in range(N):
+        p_tri(i)
+        print(*memo[i])
--- 02_algorithm/99_Extra/2w_Extra/1859_백만장자_프로젝트/sol.py
+++ 02_algorithm/99_Extra/2w_Extra/1859_백만장자_프로젝트/sol.py
@@ -5,22 +5,39 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     cost = list(map(int, input().split()))
-    ls = []
     result = 0
 
-    print(f'#{tc}', end=' ')
-    for i in cost:
+    while cost:
         max_value = max(cost)
-        for j in range(cost.index(max_value)+1):
-            if cost[j] == max_value:
-                result += len(ls) * cost[j] - sum(ls)
-                for k in range(cost.index(max_value)+1):
-                    cost.pop(0)
-                ls = []
-            else:
-                ls.append(cost[j])
+        max_index = cost.index(max(cost))
 
-    if result > 0:
-        print(result)
-    else:
-        print(0)
+        result += (max_index+1) * max_value - sum(cost[:max_index+1])
+        del cost[:max_index+1]
+
+    print(f'#{tc} {result}')
+
+
+
+    # 시간 초과
+    # ls = []
+    # result = 0
+    # i = 0
+    #
+    # print(f'#{tc}', end=' ')
+    # while i < len(cost):
+    #     max_value = max(cost)
+    #     for j in range(cost.index(max_value)+1):
+    #         if cost[j] == max_value:
+    #             result += len(ls) * cost[j] - sum(ls)
+    #             for k in range(cost.index(max_value)+1):
+    #                 cost.pop(0)
+    #             ls = []
+    #             i = 0
+    #         else:
+    #             ls.append(cost[j])
+    #             i += 1
+    #
+    # if result > 0:
+    #     print(result)
+    # else:
+    #     print(0)
