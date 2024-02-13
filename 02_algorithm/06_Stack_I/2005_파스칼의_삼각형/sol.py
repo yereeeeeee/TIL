a3f6a698f45b31e3aac09761160bcc1a8926106e
윤예리 <yoonr721@gmail.com>
2024-02-13 10:53:25 +0900

    자리이동

--- 02_algorithm/06_Stack_I/2005_파스칼의_삼각형/sol.py
+++ 02_algorithm/06_Stack_I/2005_파스칼의_삼각형/sol.py
@@ -1,29 +1,49 @@
 import sys
 sys.stdin = open('input.txt')
 
-# 메모리 에러
 T = int(input())
 for tc in range(1, T+1):
+    print(f'# {tc}')
     N = int(input())
 
     def p_tri(n):
-        global memo
-        if n >= 2:
-            result = []
-            a = []
+        if n == 0:
+            return [1]
+        elif n == 1:
+            return [1, 1]
+        else:
+            mid = []
             for i in range(n-1):
-                a.append(memo[n-1][i]+memo[n-1][i+1])
-            result.append(1)
-            result.extend(a)
-            result.append(1)
-            memo[n] = result
-            return memo[n]
+                mid.append(p_tri(n-1)[i] + p_tri(n-1)[i+1])
+            return [1, *mid, 1]
 
-    memo = [0] * (N)
-    memo[0] = [1]
-    memo[1] = [1, 1]
+    for j in range(N):
+        print(*p_tri(j))
 
-    print(f'#{tc}')
-    for i in range(N):
-        p_tri(i)
-        print(*memo[i])
+
+# 메모리 에러
+# T = int(input())
+# for tc in range(1, T+1):
+#     N = int(input())
+#
+#     def p_tri(n):
+#         global memo
+#         if n >= 2:
+#             result = []
+#             a = []
+#             for i in range(n-1):
+#                 a.append(memo[n-1][i]+memo[n-1][i+1])
+#             result.append(1)
+#             result.extend(a)
+#             result.append(1)
+#             memo[n] = result
+#             return memo[n]
+#
+#     memo = [0] * (N)
+#     memo[0] = [1]
+#     memo[1] = [1, 1]
+#
+#     print(f'#{tc}')
+#     for i in range(N):
+#         p_tri(i)
+#         print(*memo[i])
--- 02_algorithm/07_Stack_I/1219_길찾기/sol.py
+++ 02_algorithm/07_Stack_I/1219_길찾기/sol.py
@@ -1,3 +1,10 @@
 import sys
 sys.stdin = open('input.txt')
 
+def DFS():
+
+T = int(input())
+for tc in range(1, T+1):
+    length = int(input())
+    visited = [0] * length
+
