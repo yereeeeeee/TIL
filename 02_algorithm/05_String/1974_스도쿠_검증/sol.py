4c891d20988d1fe1bad8fe386e2d910335a68821
윤예리 <yoonr721@gmail.com>
2024-02-19 17:24:40 +0900

    0219

--- 02_algorithm/05_String/1974_스도쿠_검증/sol.py
+++ 02_algorithm/05_String/1974_스도쿠_검증/sol.py
@@ -1,3 +1,14 @@
 import sys
 sys.stdin = open('input.txt')
 
+T = int(input())
+for tc in range(1, T+1):
+    print(f'#{tc}', end=' ')
+    arr = [list(map(int, input().split())) for _ in range(9)]
+
+    def sdoku():
+        for i in range(9):
+            for j in range(9):
+                if j not in arr[i]:
+                    return 0
+
