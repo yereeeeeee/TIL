7707ab309f32920ac1f4b4fdfe2fd854def446c2
윤예리 <yoonr721@gmail.com>
2024-04-12 01:42:12 +0900

    0412

---
+++ Al_Study/11주차/baekjoon_1327_윤예리.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
--- Al_Study/11주차/baekjoon_14567_윤예리.py
+++ Al_Study/11주차/baekjoon_14567_윤예리.py
@@ -2,20 +2,27 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-def find(x):
-    if par[x]:
-        for y in par[x]:
-            if y not in graph[i]:
-                graph[i].append(y)
-            find(y)
-
 n, m = map(int, input().split())
-par = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+idx = [0] * (n+1)   # 진입 차수
 for _ in range(m):
     a, b = map(int, input().split())
-    par[b].append(a)    # b의 부모는 a
+    graph[a].append(b)
+    idx[b] += 1
 
-graph = [[] for _ in range(n+1)]
+q = deque([])
+
 for i in range(1, n+1):
-    find(i)
-print(graph)
+    if idx[b] == 0:
+        q.append(i)
+
+result = []
+while q:
+    now = q.popleft()
+    result.append(now)
+
+    for next_ in graph[now]:
+        idx[next_] -= 1
+        if not idx[next_]:
+            q.append(next_)
+        result
