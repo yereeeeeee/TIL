f4ae9117f7f34bcb84a8dc3951744aadc0c9fa6e
윤예리 <yoonr721@gmail.com>
2024-05-13 23:25:05 +0900

    0513_home

--- Al_Study/15주차/baekjoon_7579_윤예리.py
+++ Al_Study/15주차/baekjoon_7579_윤예리.py
@@ -1,23 +1,22 @@
+# 냅색 문제
 import sys
 input = sys.stdin.readline
 
-def dfs(i, memory, value):
-    global result
-
-    if memory >= m:
-        result = min(result, value)
-        return
-
-    if value > result:
-        return
-
-    for j in range(i, n):
-        dfs(j+1, memory+arr[j], value+cost[j])
-        dfs(j+1, memory, value)
-
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-cost = list(map(int, input().split()))
+memory = [0] + list(map(int, input().split()))
+cost = [0] + list(map(int, input().split()))
+dp = [[0] * (sum(cost)+1) for _ in range(n+1)]
 result = float('inf')
-dfs(0, 0, 0)
-print(result)
+
+for i in range(1, n+1):
+    for j in range(sum(cost)+1):
+        if j < cost[i]:   # cost 가 다 떨어지면 이전 dp
+            dp[i][j] = dp[i-1][j]
+
+        else:   # 앱 끈 거, 안 끈 거 비교
+            dp[i][j] = max(dp[i-1][j-cost[i]] + memory[i], dp[i-1][j])
+
+        if dp[i][j] >= m:
+            result = min(result, j)
+
+print(result if m else 0)
