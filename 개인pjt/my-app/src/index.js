9651419ca66bf3db419379eb9e99ad8343c1c23e
윤예리 <yoonr721@gmail.com>
2024-06-10 19:45:42 +0900

    0610

--- 개인pjt/my-app/src/index.js
+++ 개인pjt/my-app/src/index.js
@@ -4,14 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './chapter_03/Library';
+// import Library from './chapter_03/Library';
+// import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <CommentList />
   </React.StrictMode>
-);
+)
+
+
+// root.render(
+//   <React.StrictMode>
+//     <Library />
+//   </React.StrictMode>
+// );
+
+// setInterval(() => {
+//   const root = ReactDOM.createRoot(document.getElementById('root')); root.render(
+//     <React.StrictMode>
+//     <Clock />
+//     </React.StrictMode>
+//   )
+// }, 1000)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
