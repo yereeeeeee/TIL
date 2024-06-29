534ad27bf58629f96ea8164815a69ca4560b13d1
윤예리 <yoonr721@gmail.com>
2024-06-29 16:53:06 +0900

    20추가

--- Al_Study/4주차/baekjoon_1012_윤예리.py
+++ Al_Study/4주차/baekjoon_1012_윤예리.py
@@ -1,6 +1,20 @@
 import sys
 sys.setrecursionlimit(10000)
 
+# 상하좌우
+def find(i, j):
+    arr[i][j] = 0
+
+    di = [-1, 1, 0, 0]
+    dj = [0, 0, -1, 1]
+
+    for d in range(4):
+        ni = i + di[d]
+        nj = j + dj[d]
+
+        if 0<=ni<m and 0<=nj<n and arr[ni][nj] == 1:
+            find(ni, nj)
+
 T = int(input())
 for tc in range(1, T+1):
     # m: 행의 수, n: 열의 수
@@ -12,19 +26,7 @@ for tc in range(1, T+1):
         y, x = map(int, input().split())
         arr[x][y] = 1
 
-    # 상하좌우
-    def find(i, j):
-        arr[i][j] = 0
 
-        di = [-1, 1, 0, 0]
-        dj = [0, 0, -1, 1]
-
-        for d in range(4):
-            ni = i + di[d]
-            nj = j + dj[d]
-
-            if 0<=ni<m and 0<=nj<n and arr[ni][nj] == 1:
-                find(ni, nj)
 
     cnt = 0
     for i in range(m):
