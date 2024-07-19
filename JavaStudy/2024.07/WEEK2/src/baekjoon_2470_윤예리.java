74ae15fee208885766869f76003d6ab3ca8e917b
윤예리 <yoonr721@gmail.com>
2024-07-19 17:15:09 +0900

    0719

--- JavaStudy/2024.07/WEEK2/src/baekjoon_2470_윤예리.java
+++ JavaStudy/2024.07/WEEK2/src/baekjoon_2470_윤예리.java
@@ -20,30 +20,28 @@ public interface baekjoon_2470_윤예리 {
         int start = 0;
         int end = (n-1);
 
-        int[] answer = new int[2];
+        int left_answer = arr[start];
+        int right_answer = arr[end];
+        int tmp = Integer.MAX_VALUE;
 
         while (start < end) {
 
             int target = arr[start] + arr[end]; // 현재 계산하는 값
 
-            if (target == 0) break;
-            else if (target < 0) { // 값이 0 보다 작다?
-                int next_target = arr[start+1] + arr[end]; // 다음 계산할 값 확인
-
-                if (Math.abs(target) < Math.abs(next_target)) break; // 더 안 좋은 값을 가진다? 코드 종료
-                else start += 1; // 아니라면 start + 1 해주고 한 번 더 보기
+            if (target == 0) {
+                System.out.println(arr[start]+" "+arr[end]);
+                return;
             }
-            else {
-                int next_target = arr[start] + arr[end-1];
 
-                if (Math.abs(target) < Math.abs(next_target)) break;
-                else end -= 1;
+            if (tmp > Math.abs(target)) {
+                tmp = Math.abs(target);
+                left_answer = arr[start];
+                right_answer = arr[end];
             }
+
+            if (target < 0) start += 1;
+            else end -= 1;
         }
-
-
-        System.out.print(arr[start]);
-        System.out.print(" ");
-        System.out.print(arr[end]);
+        System.out.println(left_answer+" "+right_answer);
     }
 }
