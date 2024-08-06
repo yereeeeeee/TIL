7044c5b31528835fbf03c58efbe0b6b897725a04
윤예리 <yoonr721@gmail.com>
2024-08-06 17:54:54 +0900

    0806

---
+++ JavaStudy/2024.08/untitled/src/UserSolution.java
@@ -0,0 +1,23 @@
+class UserSolution {
+    void init(int N, int mSoldier[][], char mMonarch[][][])
+    {
+
+    }
+    void destroy()
+    {
+
+    }
+    int ally(char mMonarchA[], char mMonarchB[])
+    {
+
+        return -4;
+    }
+    int attack(char mMonarchA[], char mMonarchB[], char mGeneral[])
+    {
+        return -3;
+    }
+    int recruit(char mMonarch[], int mNum, int mOption)
+    {
+        return -1;
+    }
+}
