47ef9339fe7d2bc2719892168a2726a6bea62071
윤예리 <yoonr721@gmail.com>
2024-08-11 22:46:51 +0900

    0811

--- JavaStudy/2024.08/2/src/UserSolution.java
+++ JavaStudy/2024.08/2/src/UserSolution.java
@@ -1,18 +1,21 @@
-import java.io.*;
 import java.util.*;
 
 class UserSolution {
     static HashMap<Integer, PriorityQueue<Player>> bestMember = new HashMap<>();
     static HashMap<Integer, PriorityQueue<Player>> worstMember = new HashMap<>();
     static int leagueNum;
+
     void init(int N, int L, int mAbility[]) {
-        leagueNum = N/L;
+        leagueNum = N / L;
         for (int i = 0; i < leagueNum; i++) {
-            bestMember.put(i, new PriorityQueue<>());
+            // bestMember는 기본적으로 오름차순 (능력치가 큰 것이 위로 오도록)
+            bestMember.put(i, new PriorityQueue<>(Collections.reverseOrder()));
+            // worstMember는 내림차순 (능력치가 작은 것이 위로 오도록)
             worstMember.put(i, new PriorityQueue<>());
-            for (int j = i; j < L+i; j++) {
+
+            for (int j = i * L; j < (i + 1) * L; j++) {
                 bestMember.get(i).offer(new Player(j, mAbility[j]));
-                bestMember.get(i).offer(new Player(j, -1 * mAbility[j]));
+                worstMember.get(i).offer(new Player(j, mAbility[j]));
             }
         }
     }
@@ -22,20 +25,21 @@ class UserSolution {
 
         for (int i = 1; i < leagueNum - 1; i++) {
             Player bestPlayer = bestMember.get(i).peek();
-            Player worstPlayer_top = worstMember.get(i-1).peek();
+            Player worstPlayerTop = worstMember.get(i - 1).peek();
             Player worstPlayer = worstMember.get(i).peek();
-            Player bestPlayer_bot = bestMember.get(i+1).peek();
+            Player bestPlayerBot = bestMember.get(i + 1).peek();
+
+            
         }
 
-
-        return 0;
+        return sumId;
     }
 
     int trade() {
         return 0;
     }
 
-    class Player {
+    class Player implements Comparable<Player> {
         int id;
         int ability;
 
@@ -43,6 +47,10 @@ class UserSolution {
             this.id = id;
             this.ability = ability;
         }
-    }
 
+        @Override
+        public int compareTo(Player other) {
+            return Integer.compare(this.ability, other.ability);
+        }
+    }
 }
